78e85b3ce4bbfba1e165067e697134c4c0816a72
agayushhh <ayuagr15061@gmail.com>
2024-05-19 22:14:57 +0000

    Mon, May 20, 2024, 3:44 AM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -3,6 +3,14 @@
 int main()
 {
     long n = get_long("Number: ");
-    
+    int sum=0;
+    int rsn;
+    int gsn;
+    for(int i=0;i<7;i++)
+    {
+        rsn=n/10;
+        gsn=rsn%10;
+        if(gsn)
+    }
 
 }
