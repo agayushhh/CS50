122c14a31354bea6cf60b9b39bacfb387e6cbaaa
agayushhh <ayuagr15061@gmail.com>
2024-05-19 22:35:16 +0000

    Mon, May 20, 2024, 4:05 AM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -4,13 +4,16 @@ int main()
 {
     long n = get_long("Number: ");
     int sum=0;
-    long rsn = n;;
+    int sum2=0;
+    long rsn = n;
+    int gfn;
     int gsn;
     int msn;
     if ( rsn/1e14 > 0)
     {
     for(int i=0;i<9;i++)
     {
+        gfn = rsn%10;
         rsn= rsn/10;
         gsn=rsn%10;
         if((gsn*2)>=10)
