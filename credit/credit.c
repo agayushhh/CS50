7e4965d46a62cb3969c1464cb82d616c1a675abe
agayushhh <ayuagr15061@gmail.com>
2024-05-19 22:32:39 +0000

    Mon, May 20, 2024, 4:02 AM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -7,7 +7,8 @@ int main()
     long rsn = n;;
     int gsn;
     int msn;
-    if ( rsn/1e14 > 0){
+    if ( rsn/1e14 > 0)
+    {
     for(int i=0;i<9;i++)
     {
         rsn= rsn/10;
@@ -24,10 +25,43 @@ int main()
         }
         rsn= rsn/10;
     }
-    }
-    else if (rsn/1000>0)
-    {
+     if ( sum%10 ==0)
+     {
+        printf("VISA");
+     }
 
     }
+
+    else if (rsn/1e12 > 0)
+    {
+    for(int i=0;i<7;i++)
+    {
+        rsn= rsn/10;
+        gsn=rsn%10;
+        if((gsn*2)>=10)
+        {
+            msn=(2*gsn)%10;
+            sum = sum + 1 + msn;
+        }
+        else
+        {
+            msn = gsn*2;
+            sum = sum + msn;
+        }
+        rsn= rsn/10;
+    }
+     if ( sum%10 == 0)
+     {
+        printf("VISA");
+     }
+
+    }
+
+    else
+    {
+        printf("INVALID");
+    }
+
+return 0;
 
 }
