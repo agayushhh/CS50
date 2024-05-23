dc1b98f878e071cd377f9c9e37eab6cc53337394
agayushhh <ayuagr15061@gmail.com>
2024-05-23 23:39:20 +0000

    Fri, May 24, 2024, 5:09 AM +05:30

--- scrabble.c
+++ scrabble.c
@@ -1,3 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int value = {1, 3, 3, 2, 1,	,4	2,	4,	1,	8,	5,	1,	3,	1,	1,	3,	10,	1,	1,	1,	1,	4,	4,	8,	4,	10}
+int score(string s);
+
+int main()
+ {
+    string player1 = get_string("Player 1: ");
+    string player2 = get_string("Player 2: ");
+
+ }
+
+int score(string s)
+{
+
+}
