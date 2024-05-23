c929152e32cd800be66a44b30998355dd2d6b6b5
agayushhh <ayuagr15061@gmail.com>
2024-05-23 23:51:02 +0000

    Fri, May 24, 2024, 5:21 AM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -9,10 +9,23 @@ int main()
  {
     string player1 = get_string("Player 1: ");
     string player2 = get_string("Player 2: ");
-
+    if(score(player1)>score(player2))
+    {
+        printf("Player 1 wins!\n");
+    }
+    if(score(player1)<score(player2))
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
  }
 
 int score(string s)
 {
-
+    int i=0;
+    while(s[i]!=\0)
+    
 }
