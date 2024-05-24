13afae64ad48e57bd9cfdd21612ccd5e586421be
agayushhh <ayuagr15061@gmail.com>
2024-05-24 14:17:34 +0000

    Fri, May 24, 2024, 7:47 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,11 +1,17 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 int main(int argc, string argv[])
 {
     if(strlen(argv[1]) !=26)
     {
-        printf("Usage: Key should be of 26 Character");
+        printf("Usage: Key should be of 26 Character\n");
+        return 1;
+    }
+    else if(isblank(argv[1]))
+    {
+        printf("Usage: ./substitution Key\n");
         return 1;
     }
 }
