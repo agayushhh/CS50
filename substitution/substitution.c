a6154a95b1d516827dfc99f8220ddf62c761a0a1
agayushhh <ayuagr15061@gmail.com>
2024-05-24 14:20:24 +0000

    Fri, May 24, 2024, 7:50 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -9,7 +9,7 @@ int main(int argc, string argv[])
         printf("Usage: Key should be of 26 Character\n");
         return 1;
     }
-    else if(isblank(argv[1]))
+    else if(argc == 1)
     {
         printf("Usage: ./substitution Key\n");
         return 1;
