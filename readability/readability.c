24be349877460b6a6d771e0dc1b877994fc12a25
agayushhh <ayuagr15061@gmail.com>
2024-05-24 10:21:54 +0000

    Fri, May 24, 2024, 3:51 PM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -5,44 +5,42 @@
 int main()
 {
     string s = get_string("Text: ");
-    int punc=0;
-    int gap=0;
-    int sent=0;
-    int words=1;
-    for(int i=0; i<strlen(s);i++)
+    int punc = 0;
+    int gap = 0;
+    int sent = 0;
+    int words = 1;
+    for (int i = 0; i < strlen(s); i++)
     {
-        if(s[i]==".")
+        if (s[i] == '.')
         {
             sent++;
             punc++;
         }
-        else if(s[i]==",")
+        else if (s[i] == ',')
         {
             punc++;
         }
-        else if(s[i]==" ")
+        else if (s[i] == ' ')
         {
             gap++;
             words++;
         }
     }
-    float L= ((strlen(s)-gap-punc)/words)*100;
-    float S= (sent/words)*100;
+    float L = ((strlen(s) - gap - punc) / words) * 100;
+    float S = (sent / words) * 100;
     int index = 0.0588 * L - 0.296 * S - 15.8;
-    if (index>16)
+    if (index > 16)
     {
         printf("Grade 16+\n");
     }
-    else if(index<1)
+    else if (index < 1)
     {
         printf("Before Grade 1");
     }
     else
     {
-        printf("Grade %i",index);
+        printf("Grade %i", index);
     }
 
     return 0;
-
-
 }
