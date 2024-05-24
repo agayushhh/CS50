62f9b8d3f1b48b7c3167491937d082c26a02d946
agayushhh <ayuagr15061@gmail.com>
2024-05-24 22:07:01 +0000

    Sat, May 25, 2024, 3:37 AM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -52,7 +52,7 @@ int main(int argc, string argv[])
         {
             printf("%c",s[i]);
         }
-        else if(argv[1][s[i]] == '\'' || argv[1][s[i]] == '"'|| argv[1][s[i]] == '!' ||argv[1][s[i]] == ',')
+        else if(s[i] == '\'' || s[i] == '"'|| s[i] == '!' || s[i] == ',')
         {
             printf("%c",s[i]);
         }
