7835ce4fc37553f7d2915732bb3079a354642f7f
agayushhh <ayuagr15061@gmail.com>
2024-05-24 10:04:44 +0000

    Fri, May 24, 2024, 3:34 PM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -5,5 +5,25 @@
 int main()
 {
     string s = get_string("Text: ");
-    
+    int punc=0;
+    int gap=0;
+    int sent=0;
+    for(int i=0; i<strlen(s);i++)
+    {
+        if(s[i]==".")
+        {
+            sent++;
+            punc++;
+        }
+        else if(s[i]==",")
+        {
+            punc++;
+        }
+        else if(s[i]==" ")
+        {
+            gap++;
+        }
+    }
+    int L= (strlen(s)-gap-punc)/100
+    int s= sent/100
 }
