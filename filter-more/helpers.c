2749071613a28d7b091fd05b15a3806ca7e7b078
agayushhh <ayuagr15061@gmail.com>
2024-06-11 12:15:46 +0000

    Tue, Jun 11, 2024, 5:45 PM +05:30

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,7 +3,18 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    int avg_gray=0;
+    for(int i=0;i<height;i++)
+    {
+        for(int j=0;j<width;j++)
+        {
+            avg_gray=image[i][j].rgbtRed+image[i][j].rgbtBlue+image[i][j].rgbtGreen;
+            image[i][j].rgbtRed = avg_gray;
+            image[i][j].rgbtGreen = avg_gray;
+            image[i][j].rgbtBlue = avg_gray;
+
+        }
+    }
     return;
 }
 
