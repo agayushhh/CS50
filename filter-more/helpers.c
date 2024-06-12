d235f0bd47b38cd9a50320c9d3a7a2cb51f062cb
agayushhh <ayuagr15061@gmail.com>
2024-06-12 18:01:43 +0000

    Wed, Jun 12, 2024, 11:31 PM +05:30

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -56,9 +56,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
              }
            else if (i==0 && j== w-1)
            {
-                red=(image[i][j].rgbtRed+image[i-1][j].rgbtRed+image[i][j+1].rgbtRed+image[i+1][j+1].rgbtRed)/4;
-                Green=(image[i][j].rgbtGreen + image[i+1][j].rgbtGreen +image[i][j+1].rgbtGreen +image[i+1][j+1].rgbtGreen)/4;
-                Blue =(image[i][j].rgbtBlue+image[i+1][j].rgbtBlue+image[i][j+1].rgbtBlue+image[i+1][j+1].rgbtBlue)/4;
+                red=(image[i][j].rgbtRed+image[i][j-1].rgbtRed+image[i+1][j].rgbtRed+image[i+1][j-1].rgbtRed)/4;
+                Green=(image[i][j].rgbtGreen + image[i+1][j].rgbtGreen +image[i][j-1].rgbtGreen +image[i+1][j-1].rgbtGreen)/4;
+                Blue =(image[i][j].rgbtBlue+image[i+1][j].rgbtBlue+image[i][j-1].rgbtBlue+image[i+1][j-1].rgbtBlue)/4;
                 image[i][j].rgbtRed = red;
                 image[i][j].rgbtGreen = Green;
                 image[i][j].rgbtBlue = Blue;
