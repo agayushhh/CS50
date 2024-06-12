a2146d9b0ae7eec903d62708af4f7086b1751219
agayushhh <ayuagr15061@gmail.com>
2024-06-12 17:54:30 +0000

    Wed, Jun 12, 2024, 11:24 PM +05:30

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -47,7 +47,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
              }
           else if(i==0 && j!=w-1)
              {
-
+                red=(image[i][j].rgbtRed+image[i+1][j].rgbtRed+image[i][j+1].rgbtRed+image[i+1][j+1].rgbtRed)/4;
+                Green=(image[i][j].rgbtGreen + image[i+1][j].rgbtGreen +image[i][j+1].rgbtGreen +image[i+1][j+1].rgbtGreen)/4;
+                Blue =(image[i][j].rgbtBlue+image[i+1][j].rgbtBlue+image[i][j+1].rgbtBlue+image[i+1][j+1].rgbtBlue)/4;
              }
 
         }
