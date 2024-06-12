aba6bfba2b6e1539cc0831139a4771bfbf591bed
agayushhh <ayuagr15061@gmail.com>
2024-06-12 17:44:43 +0000

    Wed, Jun 12, 2024, 11:14 PM +05:30

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -27,6 +27,18 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    int h= height;
+    int w= width;
+    for(int i=0;i<h;i++)
+    {
+        for(int j=0;j<w;j++)
+        {
+         if((i==0 && j==0) ||(i== h-1 && j==0)||(i==h-1 && j==w-1)||(i==0 && j==w-1)
+            {
+                
+            }
+        }
+    }
     return;
 }
 
