24e80ae8e066f30774b68a26f7b125af92c8a411
agayushhh <ayuagr15061@gmail.com>
2024-06-13 07:15:04 +0000

    Thu, Jun 13, 2024, 12:45 PM +05:30

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -102,10 +102,10 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 image[i][j].rgbtGreen = Green;
                 image[i][j].rgbtBlue = Blue;
            }
-         else if(i==h-1 && j!=0)
+         else if(i!=0 && j==0)
              {
-                red=(image[i][j].rgbtRed+image[i-1][j].rgbtRed+image[i][j-1].rgbtRed+image[i][j+1].rgbtRed + image[i-1][j-1].rgbtRed+image[i-1][j+1].rgbtRed)/6;
-                Green=(image[i][j].rgbtGreen + image[i-1][j].rgbtGreen +image[i][j-1].rgbtGreen +image[i][j+1].rgbtGreen +image[i-1][j+1].rgbtGreen +image[i-1][j-1].rgbtGreen)/6;
+                red=(image[i][j].rgbtRed+image[i-1][j].rgbtRed+image[i+1][j].rgbtRed+image[i][j+1].rgbtRed + image[i-1][j+1].rgbtRed+image[i+1][j+1].rgbtRed)/6;
+                Green=(image[i][j].rgbtGreen + image[i-1][j].rgbtGreen +image[i][j-1].rgbtGreen +image[i][j+1].rgbtGreen +image[i-1][j+1].rgbtGreen +image[i+1][j+1].rgbtGreen)/6;
                 Blue =(image[i][j].rgbtBlue+image[i-1][j].rgbtBlue+image[i][j-1].rgbtBlue+image[i][j+1].rgbtBlue + image[i-1][j+1].rgbtBlue +image[i-1][j-1].rgbtBlue)/6;
                 image[i][j].rgbtRed = red;
                 image[i][j].rgbtGreen = Green;
