5a0394cebf38f3f0ae37b904ca3ab1a7d43fb2d4
agayushhh <ayuagr15061@gmail.com>
2024-06-19 15:59:21 +0000

    Wed, Jun 19, 2024, 9:29 PM +05:30

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -227,12 +227,15 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 
          else if(i==h-1 && j!=0)
              {
-                red=(image[i][j].rgbtRed+image[i-1][j].rgbtRed+image[i][j-1].rgbtRed+image[i][j+1].rgbtRed + image[i-1][j-1].rgbtRed+image[i-1][j+1].rgbtRed)/6;
-                Green=(image[i][j].rgbtGreen + image[i-1][j].rgbtGreen +image[i][j-1].rgbtGreen +image[i][j+1].rgbtGreen +image[i-1][j+1].rgbtGreen +image[i-1][j-1].rgbtGreen)/6;
-                Blue =(image[i][j].rgbtBlue+image[i-1][j].rgbtBlue+image[i][j-1].rgbtBlue+image[i][j+1].rgbtBlue + image[i-1][j+1].rgbtBlue +image[i-1][j-1].rgbtBlue)/6;
-                image[i][j].rgbtRed = red;
-                image[i][j].rgbtGreen = Green;
-                image[i][j].rgbtBlue = Blue;
+                gx=((image[i][j].rgbtRed * 0)+(image[i-1][j].rgbtRed*0)+(image[i+1][j].rgbtRed*0)+(image[i][j+1].rgbtRed*2) + (image[i-1][j+1].rgbtRed*1)+(image[i+1][j+1].rgbtRed*1) + (image[i][j-1].rgbtRed*(-2)) + (image[i-1][j-1].rgbtRed*(-1))+(image[i+1][j-1].rgbtRed*(-1)));
+                gy=((image[i][j].rgbtRed * 0)+(image[i-1][j].rgbtRed*(-2))+(image[i+1][j].rgbtRed*(2))+(image[i][j+1].rgbtRed*0) + (image[i-1][j+1].rgbtRed*(-1))+(image[i+1][j+1].rgbtRed*1) + (image[i][j-1].rgbtRed*(0)) + (image[i-1][j-1].rgbtRed*(-1))+(image[i+1][j-1].rgbtRed*(1)));
+                image[i][j].rgbtRed = sqrt(((gx*gx)+(gy*gy)));
+                gx=((image[i][j].rgbtGreen * 0)+(image[i-1][j].rgbtGreen*0)+(image[i+1][j].rgbtGreen*0)+(image[i][j+1].rgbtGreen*2) + (image[i-1][j+1].rgbtGreen*1)+(image[i+1][j+1].rgbtGreen*1) + (image[i][j-1].rgbtGreen*(-2)) + (image[i-1][j-1].rgbtGreen*(-1))+(image[i+1][j-1].rgbtGreen*(-1)));
+                gy=((image[i][j].rgbtGreen * 0)+(image[i-1][j].rgbtGreen*(-2))+(image[i+1][j].rgbtGreen*(2))+(image[i][j+1].rgbtGreen*0) + (image[i-1][j+1].rgbtGreen*(-1))+(image[i+1][j+1].rgbtGreen*1) + (image[i][j-1].rgbtGreen*(0)) + (image[i-1][j-1].rgbtGreen*(-1))+(image[i+1][j-1].rgbtGreen*(1)));
+                image[i][j].rgbtGreen = sqrt(((gx*gx)+(gy*gy)));
+                gx=((image[i][j].rgbtBlue * 0)+(image[i-1][j].rgbtBlue*0)+(image[i+1][j].rgbtBlue*0)+(image[i][j+1].rgbtBlue*2) + (image[i-1][j+1].rgbtBlue*1)+(image[i+1][j+1].rgbtBlue*1) + (image[i][j-1].rgbtBlue*(-2)) + (image[i-1][j-1].rgbtBlue*(-1))+(image[i+1][j-1].rgbtBlue*(-1)));
+                gy=((image[i][j].rgbtBlue * 0)+(image[i-1][j].rgbtBlue*(-2))+(image[i+1][j].rgbtBlue*(2))+(image[i][j+1].rgbtBlue*0) + (image[i-1][j+1].rgbtBlue*(-1))+(image[i+1][j+1].rgbtBlue*1) + (image[i][j-1].rgbtBlue*(0)) + (image[i-1][j-1].rgbtBlue*(-1))+(image[i+1][j-1].rgbtBlue*(1)));
+                image[i][j].rgbtBlue = sqrt(((gx*gx)+(gy*gy)));
              }
 
          else if (i== h-1 && j== 0) //lower left coorner.
