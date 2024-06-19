fe1bd9d4bdfc6ddc1347c98268cbd28411f309b8
agayushhh <ayuagr15061@gmail.com>
2024-06-19 13:08:09 +0000

    Wed, Jun 19, 2024, 6:38 PM +05:30

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -151,6 +151,104 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    int h= height;
+    int w= width;
+    int red,Green,Blue;
+    for(int i=0;i<h;i++)
+    {
+        for(int j=0;j<w;j++)
+        {
+            if(i==0 && j==0)
+             {
+                red=(image[i][j].rgbtRed+image[i+1][j].rgbtRed+image[i][j+1].rgbtRed+image[i+1][j+1].rgbtRed)/4;
+                Green=(image[i][j].rgbtGreen + image[i+1][j].rgbtGreen +image[i][j+1].rgbtGreen +image[i+1][j+1].rgbtGreen)/4;
+                Blue =(image[i][j].rgbtBlue+image[i+1][j].rgbtBlue+image[i][j+1].rgbtBlue+image[i+1][j+1].rgbtBlue)/4;
+                image[i][j].rgbtRed = red;
+                image[i][j].rgbtGreen = Green;
+                image[i][j].rgbtBlue = Blue;
+
+
+             }
+          else if(i==0 && j!=w-1)
+             {
+                red=(image[i][j].rgbtRed+image[i+1][j].rgbtRed+image[i][j+1].rgbtRed+image[i+1][j+1].rgbtRed + image[i][j-1].rgbtRed+image[i+1][j-1].rgbtRed)/6;
+                Green=(image[i][j].rgbtGreen + image[i+1][j].rgbtGreen +image[i][j+1].rgbtGreen +image[i+1][j+1].rgbtGreen +image[i+1][j-1].rgbtGreen +image[i][j-1].rgbtGreen)/6;
+                Blue =(image[i][j].rgbtBlue+image[i+1][j].rgbtBlue+image[i][j+1].rgbtBlue+image[i+1][j+1].rgbtBlue + image[i][j-1].rgbtBlue +image[i+1][j-1].rgbtBlue)/6;
+                image[i][j].rgbtRed = red;
+                image[i][j].rgbtGreen = Green;
+                image[i][j].rgbtBlue = Blue;
+             }
+        else if (i==0 && j== w-1)
+           {
+                red=(image[i][j].rgbtRed+image[i][j-1].rgbtRed+image[i+1][j].rgbtRed+image[i+1][j-1].rgbtRed)/4;
+                Green=(image[i][j].rgbtGreen + image[i+1][j].rgbtGreen +image[i][j-1].rgbtGreen +image[i+1][j-1].rgbtGreen)/4;
+                Blue =(image[i][j].rgbtBlue+image[i+1][j].rgbtBlue+image[i][j-1].rgbtBlue+image[i+1][j-1].rgbtBlue)/4;
+                image[i][j].rgbtRed = red;
+                image[i][j].rgbtGreen = Green;
+                image[i][j].rgbtBlue = Blue;
+           }
+         else if(i!=h-1 && j==w-1)
+             {
+                red=(image[i][j].rgbtRed+image[i+1][j].rgbtRed+image[i-1][j].rgbtRed+image[i+1][j-1].rgbtRed + image[i][j-1].rgbtRed+image[i-1][j-1].rgbtRed)/6;
+                Green=(image[i][j].rgbtGreen + image[i+1][j].rgbtGreen +image[i-1][j].rgbtGreen +image[i+1][j-1].rgbtGreen +image[i-1][j-1].rgbtGreen +image[i][j-1].rgbtGreen)/6;
+                Blue =(image[i][j].rgbtBlue+image[i+1][j].rgbtBlue+image[i-1][j].rgbtBlue+image[i+1][j-1].rgbtBlue + image[i-1][j-1].rgbtBlue +image[i][j-1].rgbtBlue)/6;
+                image[i][j].rgbtRed = red;
+                image[i][j].rgbtGreen = Green;
+                image[i][j].rgbtBlue = Blue;
+             }
+
+         else if (i== h-1 && j== w-1)
+           {
+                red=(image[i][j].rgbtRed+image[i][j-1].rgbtRed+image[i-1][j].rgbtRed+image[i-1][j-1].rgbtRed)/4;
+                Green=(image[i][j].rgbtGreen + image[i-1][j].rgbtGreen +image[i][j-1].rgbtGreen +image[i-1][j-1].rgbtGreen)/4;
+                Blue =(image[i][j].rgbtBlue+image[i-1][j].rgbtBlue+image[i][j-1].rgbtBlue+image[i-1][j-1].rgbtBlue)/4;
+                image[i][j].rgbtRed = red;
+                image[i][j].rgbtGreen = Green;
+                image[i][j].rgbtBlue = Blue;
+           }
+
+         else if(i==h-1 && j!=0)
+             {
+                red=(image[i][j].rgbtRed+image[i-1][j].rgbtRed+image[i][j-1].rgbtRed+image[i][j+1].rgbtRed + image[i-1][j-1].rgbtRed+image[i-1][j+1].rgbtRed)/6;
+                Green=(image[i][j].rgbtGreen + image[i-1][j].rgbtGreen +image[i][j-1].rgbtGreen +image[i][j+1].rgbtGreen +image[i-1][j+1].rgbtGreen +image[i-1][j-1].rgbtGreen)/6;
+                Blue =(image[i][j].rgbtBlue+image[i-1][j].rgbtBlue+image[i][j-1].rgbtBlue+image[i][j+1].rgbtBlue + image[i-1][j+1].rgbtBlue +image[i-1][j-1].rgbtBlue)/6;
+                image[i][j].rgbtRed = red;
+                image[i][j].rgbtGreen = Green;
+                image[i][j].rgbtBlue = Blue;
+             }
+
+         else if (i== h-1 && j== 0) //lower left coorner.
+           {
+                red=(image[i][j].rgbtRed+image[i][j+1].rgbtRed+image[i-1][j].rgbtRed+image[i-1][j+1].rgbtRed)/4;
+                Green=(image[i][j].rgbtGreen + image[i-1][j].rgbtGreen +image[i][j+1].rgbtGreen +image[i-1][j+1].rgbtGreen)/4;
+                Blue =(image[i][j].rgbtBlue+image[i-1][j].rgbtBlue+image[i][j+1].rgbtBlue+image[i-1][j+1].rgbtBlue)/4;
+                image[i][j].rgbtRed = red;
+                image[i][j].rgbtGreen = Green;
+                image[i][j].rgbtBlue = Blue;
+           }
+         else if(i!=0 && j==0)
+             {
+                red=(image[i][j].rgbtRed+image[i-1][j].rgbtRed+image[i+1][j].rgbtRed+image[i][j+1].rgbtRed + image[i-1][j+1].rgbtRed+image[i+1][j+1].rgbtRed)/6;
+                Green=(image[i][j].rgbtGreen + image[i-1][j].rgbtGreen +image[i+1][j].rgbtGreen +image[i][j+1].rgbtGreen +image[i-1][j+1].rgbtGreen +image[i+1][j+1].rgbtGreen)/6;
+                Blue =(image[i][j].rgbtBlue+image[i-1][j].rgbtBlue+image[i+1][j].rgbtBlue+image[i][j+1].rgbtBlue + image[i-1][j+1].rgbtBlue +image[i+1][j+1].rgbtBlue)/6;
+                image[i][j].rgbtRed = red;
+                image[i][j].rgbtGreen = Green;
+                image[i][j].rgbtBlue = Blue;
+             }
+        else
+        {
+                red=(image[i][j].rgbtRed+image[i-1][j].rgbtRed+image[i+1][j].rgbtRed+image[i][j+1].rgbtRed + image[i-1][j+1].rgbtRed+image[i+1][j+1].rgbtRed + image[i][j-1].rgbtRed + image[i-1][j-1].rgbtRed+image[i+1][j-1].rgbtRed)/9;
+                Green=(image[i][j].rgbtGreen + image[i-1][j].rgbtGreen +image[i+1][j].rgbtGreen +image[i][j+1].rgbtGreen +image[i-1][j+1].rgbtGreen +image[i+1][j+1].rgbtGreen+ image[i][j-1].rgbtGreen + image[i-1][j-1].rgbtGreen+image[i+1][j-1].rgbtGreen)/9;
+                Blue =(image[i][j].rgbtBlue + image[i-1][j].rgbtBlue+image[i+1][j].rgbtBlue+image[i][j+1].rgbtBlue + image[i-1][j+1].rgbtBlue +image[i+1][j+1].rgbtBlue + image[i][j-1].rgbtBlue + image[i-1][j-1].rgbtBlue +image[i+1][j-1].rgbtBlue)/9;
+                image[i][j].rgbtRed = red;
+                image[i][j].rgbtGreen = Green;
+                image[i][j].rgbtBlue = Blue;
+        }
+
+
+
+        }
+    }
+
     return;
 }
