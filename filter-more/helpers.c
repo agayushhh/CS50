b1ed3e4c12c663f2b757cf97cc1db02038069b73
agayushhh <ayuagr15061@gmail.com>
2024-06-19 13:41:09 +0000

    Wed, Jun 19, 2024, 7:11 PM +05:30

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -187,12 +187,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
              }
         else if (i==0 && j== w-1)
            {
-                red=(image[i][j].rgbtRed+image[i][j-1].rgbtRed+image[i+1][j].rgbtRed+image[i+1][j-1].rgbtRed)/4;
-                Green=(image[i][j].rgbtGreen + image[i+1][j].rgbtGreen +image[i][j-1].rgbtGreen +image[i+1][j-1].rgbtGreen)/4;
-                Blue =(image[i][j].rgbtBlue+image[i+1][j].rgbtBlue+image[i][j-1].rgbtBlue+image[i+1][j-1].rgbtBlue)/4;
-                image[i][j].rgbtRed = red;
-                image[i][j].rgbtGreen = Green;
-                image[i][j].rgbtBlue = Blue;
+                gx =((image[i][j].rgbtRed*0)+ (image[i][j-1].rgbtRed * (-1)) + (image[i+1][j].rgbtRed*0)+ (image[i+1][j-1].rgbtRed*(-1)));
+                gy = ((image[i][j].rgbtRed*0)+ (image[i][j-1].rgbtRed * (-1)) + (image[i+1][j].rgbtRed*0)+ (image[i+1][j-1].rgbtRed*(-1)));
+                image[i][j].rgbtRed = sqrt(((gx*gx)+(gy*gy)));
            }
          else if(i!=h-1 && j==w-1)
              {
