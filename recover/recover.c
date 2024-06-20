4c95bf38033f49c310b4440f7ebbff8307b70f08
agayushhh <ayuagr15061@gmail.com>
2024-06-20 13:01:26 +0000

    Thu, Jun 20, 2024, 6:31 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
    int8_t buffer[512];
    while(fread(&buffer,1,512,input)==512)
    {
-        if (buffer[0]==0xff && buffer[1]==0xd8 && buffer[0]==0xff && (buffer[0]& 0xff) == 0xe0 )
+        if (buffer[0]==0xff && buffer[1]==0xd8 && buffer[2]==0xff && (buffer[3]& 0xff) == 0xe0 )
         {
             if(count>1)
             {
@@ -43,6 +43,8 @@ int main(int argc, char *argv[])
                 fwrite(&buffer,1,512,img);
                 new = 1;
             }
+        }
+
         else if(new == 0 && count>=1)
         {
            fwrite(&buffer,1,512,img);
