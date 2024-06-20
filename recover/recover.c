4ebf6e5c8c53d48d2ad5a42816fa01b5e5245071
agayushhh <ayuagr15061@gmail.com>
2024-06-20 11:37:33 +0000

    Thu, Jun 20, 2024, 5:07 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+  if(argc != 2)
+  {
+    printf("./recover recover_file : should be the cmd line prompt\n");
+    return 1;
+
+  }
+
+  
 }
