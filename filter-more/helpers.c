09a32fd26f505c5578a23ee037b6a498549f7624
agayushhh <ayuagr15061@gmail.com>
2024-06-20 11:09:10 +0000

    Thu, Jun 20, 2024, 4:39 PM +05:30

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,5 +1,7 @@
 #include "helpers.h"
 #include <math.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -9,7 +11,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j=0;j<width;j++)
         {
-            avg_gray = round((image[i][j].rgbtRed+ image[i][j].rgbtBlue + image[i][j].rgbtGreen)/3);
+            avg_gray = round(atof((image[i][j].rgbtRed+ image[i][j].rgbtBlue + image[i][j].rgbtGreen))/3);
             image[i][j].rgbtRed = avg_gray;
             image[i][j].rgbtGreen = avg_gray;
             image[i][j].rgbtBlue = avg_gray;
