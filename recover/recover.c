95b121a4ebabc0c89627e820bf8d2d8639b9ef03
agayushhh <ayuagr15061@gmail.com>
2024-06-20 12:27:49 +0000

    Thu, Jun 20, 2024, 5:57 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -16,12 +16,14 @@ int main(int argc, char *argv[])
     printf("invalid file");
     return 1;
   }
+  int count=0;
    int8_t buffer[512];
    while(fread(&buffer,1,512,input)==512)
    {
         if (buffer[0]==0xff && buffer[1]==0xd8 && buffer[0]==0xff && (buffer[0]& 0xff) == 0xe0 )
         {
-            FIL
+            count++;
+            sprintf(img,%i30,count)
         }
    }
 
